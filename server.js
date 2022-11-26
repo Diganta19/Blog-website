58ea5a9998049dfe6dba2bb36a54d05fdb037c9e
Diganta Nayak <diganta.19032001@gmail.com>
2022-11-26 23:11:54 +0530

    made editor page

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ let initial_path = path.join(__dirname, "public");
 const app = express();
 app.use(express.static(initial_path));
 
+app.get('/editor', (req, res) => {
+  res.sendFile(path.join(initial_path, "editor.html"));
+})
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(initial_path, "home.html"));
 });
